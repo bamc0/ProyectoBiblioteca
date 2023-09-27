de8fc33dd256601451411af809a2d0e14c806fcd
bamc0 <bmunozcerezo@gmail.com>
2023-09-27 11:39:23 -0300

    Versión 1.1

--- app/src/main/java/com/example/proyectobiblioteca/MenuAdmin.java
+++ app/src/main/java/com/example/proyectobiblioteca/MenuAdmin.java
@@ -2,7 +2,9 @@ package com.example.proyectobiblioteca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuAdmin extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MenuAdmin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_admin);
     }
+
+    public void salirAdmin(View v){
+        Intent sig = new Intent(this, LoginAdmin.class);
+        startActivity(sig);
+    }
+
 }
