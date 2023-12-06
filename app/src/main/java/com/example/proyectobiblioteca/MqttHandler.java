d669e3513599379a0f4888b86239df1030aefd3a
bamc0 <bmunozcerezo@gmail.com>
2023-12-06 13:57:03 -0300

    Versión 06-12-2023

--- app/src/main/java/com/example/proyectobiblioteca/MqttHandler.java
+++ app/src/main/java/com/example/proyectobiblioteca/MqttHandler.java
@@ -14,29 +14,26 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 public class MqttHandler {
         private MqttClient client;
 
-    public MqttHandler() {
-    }
+        public MqttHandler() {
+        }
 
-    private String usuario = "androidteststiqq";
+        private String usuario = "androidteststiqq";
         private String password = "W0U2XNxCKinXaOBv";
 
         public void connect(String brokerUrl, String clientId, Context context) {
             try {
                 // persistencia de datos
                 MemoryPersistence persistence = new MemoryPersistence();
-                MqttConnectOptions options = new MqttConnectOptions();
-                options.setUserName(this.usuario);
-                options.setPassword(this.password.toCharArray());
+                MqttConnectOptions connectOptions = new MqttConnectOptions();
+                connectOptions.setUserName(this.usuario);
+                connectOptions.setPassword(this.password.toCharArray());
                 // Iniciar clietne MQTT
                 client = new MqttClient(brokerUrl, clientId, persistence);
 
                 // Opciones de conexion
-                MqttConnectOptions connectOptions = new MqttConnectOptions();
+
                 connectOptions.setCleanSession(true);
 
-
-                // Conectar al broker
-                client.connect(connectOptions);
                 client.setCallback(new MqttCallback() {
                     @Override
                     public void connectionLost(Throwable cause) {
@@ -56,6 +53,10 @@ public class MqttHandler {
 
                     }
                 });
+
+                // Conectar al broker
+                client.connect(connectOptions);
+
             } catch (MqttException e) {
                 e.printStackTrace();
             }
